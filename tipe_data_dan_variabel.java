62b8351609746b9a0e7d7e0ad05dffbb33c40f57
murvaena <murvaenaannisa@gmail.com>
2023-10-09 20:17:33 +0800

    day1

---
+++ tipe_data_dan_variabel.java
@@ -0,0 +1,20 @@
+public class  tipe_data_dan_variabel {
+
+    public static void main(String[] args) {
+      //membuat varuabel
+      String nama, alamat;
+      int usia;
+      double tinggi;
+      
+      //mengisi variabel diatas
+      nama = "murvaena";
+        System.out.println("nama: "+nama );
+      alamat = "balombong";
+      System.out.println("alamat: "+alamat );
+      tinggi = 148;
+      System.out.println("tinggi: "+ tinggi + "cm");
+      
+    }
+    
+}
+
