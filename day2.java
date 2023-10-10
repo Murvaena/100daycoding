d59c8d71b9abc0f6628cad9eaf8821155fca8056
murvaena <murvaenaannisa@gmail.com>
2023-10-10 21:07:32 +0800

    day2

---
+++ day2.java
@@ -0,0 +1,65 @@
+public class day2 {
+    public static void main(String[] args) {
+
+        
+        //tipe data primitif adalah tipe data bawaan dari bahasa pemrograman
+        // Tipe data non-primitif di Java adalah tipe data yang digunakan untuk menyimpan objek yang lebih kompleks
+   
+   
+   
+   
+
+        //tipe data primitf
+        //1. byte
+        byte data1 = 100;
+        System.out.println(data1);//menampilkan
+
+        //2. short
+        short data2 = 5000;
+        System.out.println(data2);//menampilkan
+
+        //3.long
+        long data3 = 123456789L;
+        System.out.println(data3);//menampilkan
+
+        //4.float
+        float data4 = 5.75f;  //tipe data float harus disertakan f di akhir untuk menunjukkan tipe data float
+        System.out.println(data4);//menampilkan
+
+        //5.double
+        double tinggiBadan = 1.47;
+        System.out.println("tinggi badan saya: "+ tinggiBadan);//menampilkan
+
+        //6.integer
+        int umur = 20;
+        System.out.println("umur saya: "+ umur);//menampilkan
+
+        //7.character
+        char kelas = 'c';
+        System.out.println(kelas);
+
+        boolean statusPernikahan= false;
+        System.out.println(statusPernikahan);
+
+        //8
+
+
+
+
+
+        //non primitif 
+         // Menggunakan tipe data non-primitif String
+                String nama = "murvaena";
+                System.out.println("Nama: " + nama);//menampilkan
+
+
+        
+
+            }
+        
+        
+
+    }
+
+    
+
