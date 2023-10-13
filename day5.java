5fb0ba18ba1d3716a43a1a220aadb2830ac2d10a
murvaena <murvaenaannisa@gmail.com>
2023-10-13 20:10:26 +0800

    day5

---
+++ day5.java
@@ -0,0 +1,31 @@
+public class day5 {
+    public static void main(String[] args) {
+
+        int angka1 = 3;
+        int angka2 = 4;
+        
+
+        //pembagian
+        int hasiBagi = angka1 % angka2;
+        System.out.println("hasil pembagian: " +hasiBagi);
+
+        //perkalian
+        int hasilKali = angka1 * angka2;
+        System.out.println("hasil perkalian: "+hasilKali);
+
+        //penjumlahan
+        int hasilPenjumlahan = angka1 + angka2;
+        System.out.println("hasil penjumlahan: "+hasilPenjumlahan);
+
+        //pengurangan
+        int hasilPengurangan = angka1 - angka2;
+        System.out.println("hasil pengurangan: "+hasilPengurangan);
+
+        //sisa bagi /
+        int sisaBagi = angka1/angka2;
+        System.out.println("sisa bagi: "+sisaBagi);
+     }
+
+    }
+
+     
