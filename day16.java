3fc72364a8babab5a2cc5e2866e0340d9dec269c
murvaena <99964711+Murvaena@users.noreply.github.com>
2023-10-24 21:47:55 +0800

    operator aritmetika
    operator aritmetika

---
+++ day16.java
@@ -0,0 +1,43 @@
+public class day16{
+    public static void main(String[] args) {
+        
+        //belajar operator aritmetika 
+
+
+        //perkalian
+        double keliling, panjang, lebar;
+
+        panjang = 5.0;
+        lebar = 7.0;
+
+        keliling = panjang * lebar;
+        System.out.println("hasilnya perkaliannya adalah : "+keliling);
+
+        //pembagian
+        int nilai1= 6;
+        int nilai2=2;
+        int pembagian;
+
+        pembagian = nilai1 / nilai2;
+        System.out.println("hasil  pembagiannya adalah: "+pembagian);
+
+
+        //sisa bagi 
+         int angka1= 6;
+        int angka2=4;
+        int sisabagi;
+
+        sisabagi = angka1 % angka2;
+        System.out.println("hasilnya  adalah: "+sisabagi);
+
+
+
+
+
+
+
+
+
+
+        }
+    }
