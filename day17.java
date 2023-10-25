b279ac845a3643f97eee00ef1fa6d2ede8d58497
murvaena <99964711+Murvaena@users.noreply.github.com>
2023-10-25 21:02:27 +0800

    operator penugasan
    operator penugasan

---
+++ day17.java
@@ -0,0 +1,24 @@
+public class day17{
+ 
+  public static void main(String[]args){
+
+
+    //belajar operator penugasan
+ 
+      int a = 20;
+      System.out.println(a+=10); //artinya sama dengan 20+10
+ 
+      int b = 10;
+      System.out.println(b-=9); //artinya sama dengan 10-9
+ 
+      int c = 5;
+      System.out.println(c*=8); //artinya perkalian 5*8
+ 
+      float d = 10;
+      System.out.println(d/=7); //artinya pembagian 10/7
+ 
+      int e = 10;
+      System.out.println(e%=8); //artinya sisa bagi 10 % 6 
+ 
+      }
+  }
