82d550223033697fb6fcd93ad4ffb38c9f10100d
murvaena <99964711+Murvaena@users.noreply.github.com>
2023-10-31 20:21:48 +0800

    perulangan for each
    perulangan for each

---
+++ day23.java
@@ -0,0 +1,14 @@
+public class day23{
+    public static void main(String[] args) {
+
+        //perulangan for each 
+        
+        // membuat array
+        String  nama[] = {"vaena ,aisyah ,nengsi,nismayanti"};
+
+        // menggunakan perulangan For each untuk menampilkan angka
+        for( String  teman : nama ){
+            System.out.print(teman + " ");
+        }
+    }
+}
