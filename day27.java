e2454c1df4e9070eb31f8c753da940214961cef1
murvaena <99964711+Murvaena@users.noreply.github.com>
2023-11-04 19:54:24 +0800

    perulangan
    perulangan

---
+++ day27.java
@@ -0,0 +1,25 @@
+public class day27{
+    public static void main(String[] args) {
+
+        //perulangan break continue
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("perulangan " + i);
+            
+            // Menggunakan break untuk menghentikan perulangan ketika i mencapai 3
+            if (i == 3) {
+                System.out.println("Perulangan dihentikan  ");
+                break;
+            }
+            
+            // Menggunakan continue untuk melanjutkan ke perulangan berikutnya ketika i adalah 2
+            if (i == 2) {
+                System.out.println("Perulangan dilanjutkan ");
+                continue;
+
+                 }
+              }
+            }
+        }
+        
+      
