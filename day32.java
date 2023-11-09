8087f53af114f9e913e9a32a8c40589a37ac4ff5
murvaena <99964711+Murvaena@users.noreply.github.com>
2023-11-09 20:54:58 +0800

    array2d
    array 2D

---
+++ day32.java
@@ -0,0 +1,19 @@
+public class day32{
+
+   public static void main (String args[]){
+    //array2d
+    //Deklarasi array
+    String[][]huruf = {
+        {"A","B","C"},//index 0
+        {"D","E","F"},//index 1
+        {"G","H","I"} // index 2
+    };
+    //Menampilkan isi array pada indeks ke-[0][1], [1][2] dan [2][0]
+    System.out.println(huruf[0][1]);
+    System.out.println(huruf[2][0]);
+    System.out.println(huruf[1][2]);
+
+
+        }
+    }
+ 
