528e982a6a5cb65acacc120891fc66364ebd96a6
murvaena <99964711+Murvaena@users.noreply.github.com>
2023-11-15 22:22:31 +0800

    array 2d
    array 2d

---
+++ day38.java
@@ -0,0 +1,19 @@
+public class day38{
+
+   public static void main (String args[]){
+    //array2d
+    //Deklarasi array
+    String[][]teman = {
+        {"Ani","Budi","Cici"},//index 0
+        {"nia","suci ","cinta"},//index 1
+        {"ica","nengsig","nisma"} // index 2
+    };
+    //Menampilkan isi array pada indeks ke-[0][1], [1][2] dan [2][0]
+    System.out.println(teman[0][1]);
+    System.out.println(teman[2][0]);
+    System.out.println(teman[1][2]);
+
+
+        }
+    }
+ 
