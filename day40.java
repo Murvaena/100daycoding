86898c968e49cb87af6289dc67703c773240d248
murvaena <99964711+Murvaena@users.noreply.github.com>
2023-11-17 21:30:28 +0800

    menghitung L  & K segitiga
    menghitung L  & K segitiga

---
+++ day40.java
@@ -0,0 +1,29 @@
+public class day40 {
+    public static void main(String[] args) {
+        
+        //segitiga 
+        //k = s + s+ s
+        //L= 0.5 x a x t
+
+        double L, K, a, s, t;
+
+        a =10;
+        t = 12;
+        s = a;
+
+
+        //menghitung keliling dan luas 
+
+        L = 0.5 * (a * t);
+        K = s + s + s;
+
+        //menampilkan 
+        System.out.println("L = "+L);
+        System.out.println("K = "+K );
+        
+
+
+
+    }
+    
+}
