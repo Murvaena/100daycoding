9997267079a378783daaa9b18629a2d00689cbcb
murvaena <99964711+Murvaena@users.noreply.github.com>
2023-11-24 22:45:12 +0800

    array
    array

---
+++ day47.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class day47 {
+
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> arrayList = new ArrayList<>()
+      
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        
+        System.out.println("jumlah elemen dalam ArrayList: " + arrayList.size());
+    
+        System.out.println("Elemen dalam ArrayList: " + arrayList);
+    }
+}
