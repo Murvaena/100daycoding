c456a3cb556c4023c5955366beab67f6f5e808f4
murvaena <99964711+Murvaena@users.noreply.github.com>
2023-12-11 22:30:43 +0800

    array2d
    array2d

---
+++ day64.java
@@ -0,0 +1,15 @@
+public class day64 {
+    public static void main(String[] args) {
+
+        int[][] a = {
+            {1, 2, 3},
+            {4, 5, 6, 9},
+            {7},
+        };
+
+        // calculate the length of each row
+        System.out.println("Panjang baris 1: " + a[0].length);
+        System.out.println("Panjang baris 2: " + a[1].length);
+        System.out.println("Panjang baris 3: " + a[2].length);
+    }
+}
